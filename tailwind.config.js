3f237fc6edd7d076e92d598c3b80f66ae3d193e5
ayman <aymansafwat2468@gmail.com>
2026-02-28 01:57:09 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",       // main HTML entry
+    "./src/**/*.{js,ts,jsx,tsx}" // all your React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
